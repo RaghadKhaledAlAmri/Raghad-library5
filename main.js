86bcb1e95524dbb4338e712d867e97084d34315b
RaghadKhaledAlAmri <raghadrose55@gmail.com>
2025-09-18 07:55:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+
+// alert('WELCOME TO THE GRATE LIBRARY 😁📖')
+
+
+
+
+
+
+let color=true
+function changeColor(){
+    if(color){
+        document.getElementById.style.backgroundColor='blue';
+        color=false
+    }
+
+    else{
+        document.getElementById.style.backgroundColor='pink';
+        color=true
+    } 
+}
+
+// const changerBtn=document.getElementById('changeColor')
+// let isLightMod=true
+// function changePageColor(){
+//     const body=document.body
+
+//     if (isLightMod){
+//         body.style.backgrondColor='rgb(65, 1, 41)'
+//         body.style.color='white'
+//         isLightMod=false
+//         changerBtn.textContent="فاتح"
+//     }
+//     else{
+//         body.style.backgrondColor='rgb(234, 151, 198)'
+//         body.style.color='black'
+//         isLightMod=true
+//         changerBtn.textContent="داكن"
+//     }
+// }
